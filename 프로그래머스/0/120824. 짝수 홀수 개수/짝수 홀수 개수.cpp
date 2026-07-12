ee4dcea9a98f55e70eee3ab82ee5241ef45fd310
이가현 <dlrkgus0858@gmail.com>
2026-07-12 22:15:25 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    
+    int even_count = 0; 
+    int odd_count = 0;
+    for(int i=0;i<num_list.size();i++){
+        if(num_list[i] % 2 == 0) {
+            even_count++; 
+        } else {
+            odd_count++;  
+        }
+    }
+    
+    answer.push_back(even_count); 
+    answer.push_back(odd_count);
+    return answer;
+}

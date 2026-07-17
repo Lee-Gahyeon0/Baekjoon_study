cadccb069d091c3dce0fe356b0bcf94f29d5adc1
이가현 <dlrkgus0858@gmail.com>
2026-07-18 02:34:27 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -4,5 +4,6 @@
 using namespace std;
 
 vector<int> solution(vector<int> numbers, int num1, int num2) {
-    return vector<int>(numbers.begin() + num1, numbers.begin() + num2 + 1);
+    vector<int> answer;
+    return answer;
 }

8a8d74de881a0c3747665a1b16ebf00c35d8d117
이가현 <dlrkgus0858@gmail.com>
2026-07-27 21:19:10 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    string even= "",odd ="";
+    
+    for(int num: num_list){
+        if(num%2==0){
+            even+=to_string(num);
+        }
+        else{
+            odd+=to_string(num);
+        }
+    }
+    return stoi(even) + stoi(odd);
+}
